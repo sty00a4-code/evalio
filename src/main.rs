f22752aea0148c50fb58bc46ddcb10011dd14da6
sty00a4-code <134289253+sty00a4-code@users.noreply.github.com>
2023-09-19 19:26:42 +0200

    ignore panic if not debug build

--- src/main.rs
+++ src/main.rs
@@ -38,25 +38,29 @@ pub fn eval(input: &str, program: &mut Program) -> Result<Option<Value>, Located
 }
 
 fn main() {
-    let mut program = Program::init();
-    let args_string = std::env::args().skip(1).collect::<Vec<String>>().join(" ");
-    if !args_string.is_empty() {
-        match eval(&args_string, &mut program) {
-            Ok(Some(value)) => println!("{value}"),
-            Err(Located { value: err, pos }) => println!("ERROR: {err}"),
-            _ => {}
+    #[cfg(not(debug_assertions))]
+    std::panic::set_hook(Box::new(|_info| {}));
+    std::panic::catch_unwind(|| {
+        let mut program = Program::init();
+        let args_string = std::env::args().skip(1).collect::<Vec<String>>().join(" ");
+        if !args_string.is_empty() {
+            match eval(&args_string, &mut program) {
+                Ok(Some(value)) => println!("{value}"),
+                Err(Located { value: err, pos }) => println!("ERROR: {err}"),
+                _ => {}
+            }
+            return
         }
-        return
-    }
-    loop {
-        let mut input = String::new();
-        print!("> ");
-        stdout().flush();
-        stdin().read_line(&mut input);
-        match eval(&input, &mut program) {
-            Ok(Some(value)) => println!("{value}"),
-            Err(Located { value: err, pos }) => println!("ERROR: {err}"),
-            _ => {}
+        loop {
+            let mut input = String::new();
+            print!("> ");
+            stdout().flush();
+            stdin().read_line(&mut input);
+            match eval(&input, &mut program) {
+                Ok(Some(value)) => println!("{value}"),
+                Err(Located { value: err, pos }) => println!("ERROR: {err}"),
+                _ => {}
+            }
         }
-    }
+    });
 }
